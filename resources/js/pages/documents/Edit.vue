53bed45e6e12d7d5d252b7cae4d2f4cd5fa7a853
johnny <gianidacians4@gmail.com>
2025-09-19 18:04:37 +0200

    Fix empty Edit.vue component for deployment

---
+++ resources/js/pages/documents/Edit.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Edit Document</h1>
+        <!-- TODO: Implement edit functionality -->
+    </div>
+</template>
+
+<script setup lang="ts">
+// TODO: Add edit document logic
+</script>
